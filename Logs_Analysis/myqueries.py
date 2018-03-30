5edcd0a3e25d7a2ebd185addc907c5afb2f57932
Jinne Kim <34400248+westjinne@users.noreply.github.com>
2018-03-30 13:53:04 +0900

    Update myqueries.py

--- Logs_Analysis/myqueries.py
+++ Logs_Analysis/myqueries.py
@@ -14,14 +14,15 @@ def get_data(query):
 
 
 def most_popular_three_articles():
-    query01 = (
-            "SELECT articles.title, count(*) AS PV "
-            "FROM articles, log "
-            "WHERE articles.slug = substr(log.path,10) "
-            "AND log.status = '200 OK' "
-            "GROUP BY articles.title "
-            "ORDER BY PV DESC "
-            "LIMIT 3;")
+    query01 = """
+            SELECT articles.title, count(*) AS PV
+            FROM articles, log
+            WHERE '/article/' || articles.slug = log.path
+            AND log.status = '200 OK'
+            GROUP BY articles.title
+            ORDER BY PV DESC
+            LIMIT 3;
+            """
     result = get_data(query01)
     print("<The most popular three articles of all time>")
     print
@@ -30,16 +31,17 @@ def most_popular_three_articles():
 
 
 def most_popular_article_authors():
-    query02 = (
-            "SELECT name, PV FROM "
-            "(SELECT authors.name, count(*) as PV "  # Set PV
-            "FROM articles, authors, log "
-            "WHERE substr(log.path,10) = articles.slug "
-            "AND log.status = '200 OK' "
-            "GROUP BY authors.name) AS subq01 "
-            "WHERE PV>1000000 "  # select PV > 1000000
-            "ORDER BY PV "
-            "DESC;")
+    query02 = """
+            SELECT name, PV FROM
+            (SELECT authors.name, count(*) as PV
+            FROM articles, authors, log
+            WHERE substr(log.path,10) = articles.slug 
+            AND log.status = '200 OK'
+            GROUP BY authors.name) AS subq01
+            WHERE PV>1000000
+            ORDER BY PV
+            DESC;
+            """
     result = get_data(query02)
     print("<The most popular article authors of all time>")
     print
@@ -48,24 +50,27 @@ def most_popular_article_authors():
 
 
 def the_days_that_requests_lead_to_error():
-    query03_01 = (
-            "SELECT count(*) AS error, date(log.time) "
-            "FROM log "
-            "WHERE log.status = '404 NOT FOUND' "
-            "GROUP BY date(log.time) "
-            "ORDER BY date(log.time) ASC;")
+    query03_01 = """
+            SELECT count(*) AS error, date(log.time)
+            FROM log
+            WHERE log.status = '404 NOT FOUND'
+            GROUP BY date(log.time)
+            ORDER BY date(log.time) ASC;
+            """
     error = get_data(query03_01)
 
-    query03_02 = (
-        "SELECT count(*) AS total, date(log.time) "
-        "FROM log "
-        "GROUP BY date(log.time);")
+    query03_02 = """
+        SELECT count(*) AS total, date(log.time)
+        FROM log
+        GROUP BY date(log.time);
+        """
     total = get_data(query03_02)
 
-    query03_03 = (
-        "SELECT date(log.time) "
-        "FROM log "
-        "GROUP BY date(log.time);")
+    query03_03 = """
+        SELECT date(log.time)
+        FROM log
+        GROUP BY date(log.time);
+        """
     result03_03 = get_data(query03_03)
 
     print ("<The days more than 1% of requests lead to errors>")
